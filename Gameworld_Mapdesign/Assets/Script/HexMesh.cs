a207b0c73cd57f41e4bac22a44090b5c41af6468
Yufei Han <hyf4053@hotmail.com>
2018-03-10 14:17:28 +0800

    Basic Hex Grid And Mesh

---
+++ Gameworld_Mapdesign/Assets/Script/HexMesh.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour {
+    /*
+     * 创建一个Mesh
+     * 创建顶点和三角形的List
+     */
+    Mesh hexMesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+
+    void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+        hexMesh.name = "Hex Mesh";
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+    }
+
+    //三角化
+    public void Triangulate (HexCell[] cells)
+    {
+        hexMesh.Clear();
+        vertices.Clear();
+        triangles.Clear();
+        for(int i = 0; i < cells.Length; i++)
+        {
+            Triangulate(cells[i]);
+        }
+        hexMesh.vertices = vertices.ToArray();
+        hexMesh.triangles = triangles.ToArray();
+        hexMesh.RecalculateNormals();
+    }
+
+    void Triangulate (HexCell cell)
+    {
+        Vector3 center = cell.transform.localPosition;
+        for (int i = 0; i < 6; i++)
+        {
+            AddTriangle(
+                center,
+                center + HexMetrics.corners[i],
+                center + HexMetrics.corners[i + 1]
+            );
+        }
+    }
+
+    void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        int vertexIndex = vertices.Count;
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
+        triangles.Add(vertexIndex);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 2);
+    }
+}
